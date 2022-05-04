2e1bc387b88c781c1757d69b1e04d9f254c2c740
Thibaud Turlan <45849917+ThibaudTurlan@users.noreply.github.com>
2022-05-04 11:04:50 +0200

    fixe: change time format

--- src/components/StopWatch.vue
+++ src/components/StopWatch.vue
@@ -84,7 +84,7 @@ export default {
         this.zeroPrefix(hour, 2) + ":" + 
         this.zeroPrefix(min, 2) + ":" + 
         this.zeroPrefix(sec, 2) + "," + 
-        this.zeroPrefix(ms, 2);
+        this.zeroPrefix(ms, 3);
       
     },
     zeroPrefix(num, digit) {
