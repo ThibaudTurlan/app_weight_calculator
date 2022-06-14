3e119e1422be33659a1633823341ec4875d30ccc
Thibaud Turlan <45849917+ThibaudTurlan@users.noreply.github.com>
2022-06-14 17:29:00 +0200

    feat: change favicon

--- vue.config.js
+++ vue.config.js
@@ -8,3 +8,23 @@ module.exports = {
     ? '/app_weight_calculator/'
     : '/'
 }
+
+module.exports = {
+  pwa: {
+    name: 'Tools 2 Sport',
+    themeColor: '#4DBA87',
+    theme_color: "#4DBA87",
+    msTileColor: '#000000',
+    background_color: '#ffffff',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: 'default',
+
+    // configure the workbox plugin
+    workboxPluginMode: 'GenerateSW',
+    // workboxOptions: {
+    //   // swSrc is required in InjectManifest mode.
+    //   swSrc: 'dev/sw.js',
+    //   // ...other Workbox options...
+    // }  
+  }
+}
